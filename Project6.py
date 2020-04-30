1b172d8431eaaee3571ddcc7111a753e55cd6c92
Jamp-H <jmh824@nau.edu>
2020-04-30 16:37:35 -0700

    Working dense NN

--- Project6.py
+++ Project6.py
@@ -26,13 +26,13 @@ import numpy as np
 ## See DOC
 
 def getNormX(data):
-    xValuesUnscaled = data[:,:-1]
+    xValuesUnscaled = data[:,1:]
     X_sc = scale(xValuesUnscaled)
     return X_sc
 
 
 def getY(data):
-    return data[:,-1]
+    return data[:,0]
 
 # Function: conventional
 # INPUT ARGS:
@@ -42,51 +42,47 @@ def getY(data):
 # num_epochs - integer value for the number of epochs wanted
 # data_set - string value decribing the data set being ran (Test, Train, Subtrain, ect.)
 # Return: array of history objects containing data about the models created
-def run_single_layered_NN(X_mat, y_vec, val_data, hidden_layers, num_epochs, data_set):
+def run_NN(X_mat, y_vec, hidden_value_list , num_epochs, data_set):
     # set model variable to keep track on which number model is being ran
-    model_number = 1
 
-    # list of colors for hidden layers
+# create a neural network with 1 hidden layer
 
-    # creat list of model data
-    model_data_list = []
+    # set model for single layered NN
+    model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(np.size(X_mat, 1), )), # input layer
+    keras.layers.Dense(hidden_value_list[0], activation='relu', use_bias=False), # hidden layer
+    keras.layers.Dense(hidden_value_list[1], activation='relu', use_bias=False), # hidden layer
+    keras.layers.Dense(hidden_value_list[2], activation='relu', use_bias=False), # hidden layer
+    keras.layers.Dense(10, activation='softmax', use_bias=False) # output layer
+    ])
 
-    # create a neural network with 1 hidden layer
-    for hidden_layer in hidden_layers:
-        # set model for single layered NN
-        model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(np.size(X_mat, 1), )), # input layer
-        keras.layers.Dense(hidden_layer, activation='sigmoid', use_bias=False), # hidden layer
-        keras.layers.Dense(1, activation='sigmoid', use_bias=False) # output layer
-        ])
+    # compile the models
+    model.compile(optimizer='adadelta',
+                  loss= "sparse_categorical_crossentropy",
+                  metrics=['accuracy'])
 
-        # compile the models
-        model.compile(optimizer='sgd',
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
-
-        # fit the models
-        print(f"\nModel {model_number} {data_set}")
-        print("==============================================")
-        model_data = model.fit(
-                                    x=X_mat,
-                                    y=y_vec,
-                                    epochs=num_epochs,
-                                    verbose=0,
-                                    validation_split=0.2)
-
-        # update model number
-        model_number += 1
-
-        # apend model data to list
-        model_data_list.append(model_data)
+    # fit the models
+    print(f"\nModel {data_set}")
+    print("==============================================")
+    model_data = model.fit(
+                                x=X_mat,
+                                y=y_vec,
+                                epochs=num_epochs,
+                                batch_size=128,
+                                verbose=2,
+                                validation_split=0.2)
 
 
 
-    return model_data_list
+
+    return model_data
 
 
 def main():
+
+    # initilize variables
+    num_epochs = 50
+    hidden_values = [270, 270, 128]
 
     np.random.seed(0)
 
@@ -97,19 +93,25 @@ def main():
 
     y_vec = getY(zip_train)
 
+    print(y_vec.shape)
+
     # reshape so each row of matrix is a 16x16 matrix
-    X_sc = np.reshape(X_sc[:,:], (X_sc.shape[0], 16, 16))
+    # X_sc = np.reshape(X_sc[:,:], (X_sc.shape[0], 16, 16))
 
     fold_ids = np.arange(5)
 
     fold_vec = np.random.permutation(np.tile(fold_ids,len(y_vec))[:len(y_vec)])
 
     for fold_num in fold_ids:
-        test_mat = X_sc[fold_num == fold_vec]
-        test_vec = y_vec[fold_num == fold_vec]
+        x_train = X_sc[fold_num != fold_vec]
+        y_train = y_vec[fold_num != fold_vec]
 
-        train_mat = X_sc[fold_num != fold_vec]
-        train_vec = y_vec[fold_num != fold_vec]
+        x_test = X_sc[fold_num == fold_vec]
+        y_test = y_vec[fold_num == fold_vec]
+        print(np.size(x_train,1))
+        run_NN(x_train, y_train, hidden_values, num_epochs, "Training")
+
+
 
 
 
